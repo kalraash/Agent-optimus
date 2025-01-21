41a799d40a81a983f7563bdac975b66ec95c4421
DivyaNautiyal07 <divyanautiyal07@gmail.com>
2025-01-21 19:29:12 +0530

    fix: add test for vanity tx

--- packages/valory/skills/liquidity_trader_abci/behaviours.py
+++ packages/valory/skills/liquidity_trader_abci/behaviours.py
@@ -806,45 +806,45 @@ class CallCheckpointBehaviour(
         with self.context.benchmark_tool.measure(self.behaviour_id).local():
             checkpoint_tx_hex = None
             min_num_of_safe_tx_required = None
+            self.service_staking_state = StakingState.STAKED
+            # if not self.params.staking_chain:
+            #     self.context.logger.warning("Service has not been staked on any chain!")
+            #     self.service_staking_state = StakingState.UNSTAKED
+            # else:
+            #     yield from self._get_service_staking_state(
+            #         chain=self.params.staking_chain
+            #     )
+            #     if self.service_staking_state == StakingState.STAKED:
+            #         min_num_of_safe_tx_required = (
+            #             yield from self._calculate_min_num_of_safe_tx_required(
+            #                 chain=self.params.staking_chain
+            #             )
+            #         )
+            #         if min_num_of_safe_tx_required is None:
+            #             self.context.logger.error(
+            #                 "Error calculating min number of safe tx required."
+            #             )
+            #         else:
+            #             self.context.logger.info(
+            #                 f"The minimum number of safe tx required to unlock rewards are {min_num_of_safe_tx_required}"
+            #             )
+            #         is_checkpoint_reached = (
+            #             yield from self._check_if_checkpoint_reached(
+            #                 chain=self.params.staking_chain
+            #             )
+            #         )
+            #         if is_checkpoint_reached:
+            #             self.context.logger.info(
+            #                 "Checkpoint reached! Preparing checkpoint tx.."
+            #             )
+            #             checkpoint_tx_hex = yield from self._prepare_checkpoint_tx(
+            #                 chain=self.params.staking_chain
+            #             )
+            #     elif self.service_staking_state == StakingState.EVICTED:
+            #         self.context.logger.error("Service has been evicted!")
 
-            if not self.params.staking_chain:
-                self.context.logger.warning("Service has not been staked on any chain!")
-                self.service_staking_state = StakingState.UNSTAKED
-            else:
-                yield from self._get_service_staking_state(
-                    chain=self.params.staking_chain
-                )
-                if self.service_staking_state == StakingState.STAKED:
-                    min_num_of_safe_tx_required = (
-                        yield from self._calculate_min_num_of_safe_tx_required(
-                            chain=self.params.staking_chain
-                        )
-                    )
-                    if min_num_of_safe_tx_required is None:
-                        self.context.logger.error(
-                            "Error calculating min number of safe tx required."
-                        )
-                    else:
-                        self.context.logger.info(
-                            f"The minimum number of safe tx required to unlock rewards are {min_num_of_safe_tx_required}"
-                        )
-                    is_checkpoint_reached = (
-                        yield from self._check_if_checkpoint_reached(
-                            chain=self.params.staking_chain
-                        )
-                    )
-                    if is_checkpoint_reached:
-                        self.context.logger.info(
-                            "Checkpoint reached! Preparing checkpoint tx.."
-                        )
-                        checkpoint_tx_hex = yield from self._prepare_checkpoint_tx(
-                            chain=self.params.staking_chain
-                        )
-                elif self.service_staking_state == StakingState.EVICTED:
-                    self.context.logger.error("Service has been evicted!")
-
-                else:
-                    self.context.logger.error("Service has not been staked")
+            #     else:
+            #         self.context.logger.error("Service has not been staked")
 
             tx_submitter = self.matching_round.auto_round_id()
             payload = CallCheckpointPayload(
@@ -933,30 +933,34 @@ class CheckStakingKPIMetBehaviour(LiquidityTraderBaseBehaviour):
         """Do the action."""
         with self.context.benchmark_tool.measure(self.behaviour_id).local():
             vanity_tx_hex = None
-            is_staking_kpi_met = yield from self._is_staking_kpi_met()
+            # is_staking_kpi_met = yield from self._is_staking_kpi_met()
+            is_staking_kpi_met = False
             if is_staking_kpi_met is None:
                 self.context.logger.error("Error checking if staking KPI is met.")
             elif is_staking_kpi_met is True:
                 self.context.logger.info("KPI already met for the day!")
             else:
-                is_period_threshold_exceeded = (
-                    self.synchronized_data.period_count
-                    - self.synchronized_data.period_number_at_last_cp
-                    >= self.params.staking_threshold_period
-                )
+                # is_period_threshold_exceeded = (
+                #     self.synchronized_data.period_count
+                #     - self.synchronized_data.period_number_at_last_cp
+                #     >= self.params.staking_threshold_period
+                # )
+                is_period_threshold_exceeded = True
 
                 if is_period_threshold_exceeded:
-                    min_num_of_safe_tx_required = (
-                        self.synchronized_data.min_num_of_safe_tx_required
-                    )
-                    multisig_nonces_since_last_cp = (
-                        yield from self._get_multisig_nonces_since_last_cp(
-                            chain=self.params.staking_chain,
-                            multisig=self.params.safe_contract_addresses.get(
-                                self.params.staking_chain
-                            ),
-                        )
-                    )
+                    # min_num_of_safe_tx_required = (
+                    #     self.synchronized_data.min_num_of_safe_tx_required
+                    # )
+                    # multisig_nonces_since_last_cp = (
+                    #     yield from self._get_multisig_nonces_since_last_cp(
+                    #         chain=self.params.staking_chain,
+                    #         multisig=self.params.safe_contract_addresses.get(
+                    #             self.params.staking_chain
+                    #         ),
+                    #     )
+                    # )
+                    min_num_of_safe_tx_required = 1
+                    multisig_nonces_since_last_cp = 0
                     if (
                         multisig_nonces_since_last_cp is not None
                         and min_num_of_safe_tx_required is not None
